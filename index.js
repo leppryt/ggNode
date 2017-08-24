b957f9d790e9896ada6d4f9ed564f48984e8266a
Philip Van Inting <intng.philipvan@gmail.com>
2017-08-25 00:26:56 +0800

    update console logs

--- index.js
+++ index.js
@@ -68,7 +68,7 @@ var reconClient = function(socket, id){
 	socket.id = id;
 	var client = clients[id.trim()];
 	if(client)
-			client.socket = socket;		
+		client.socket = socket;		
 	else
 		console.log("not found on clients list");
 	return client;
